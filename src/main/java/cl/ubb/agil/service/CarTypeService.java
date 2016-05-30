095e633451cf182c1f8d94e9baed0460261e0c7c
MiguelCastilloVasquez <m.castillo.vas@gmail.com>
2016-05-30 16:15:51 -0400

    Test Clase CarTypeService

--- src/main/java/cl/ubb/agil/service/CarTypeService.java
+++ src/main/java/cl/ubb/agil/service/CarTypeService.java
@@ -5,15 +5,18 @@ import java.util.List;
 
 import cl.ubb.agil.dao.CarTypeDao;
 import cl.ubb.agil.model.CarType;
+import cl.ubb.agil.service.exception.EmptyListException;
 
 public class CarTypeService {
 	
 	private CarTypeDao carTypeDao;
 	
-	public List <CarType> getAllCarTypes(){
+	public List <CarType> getAllCarTypes() throws EmptyListException{
 		List <CarType> carsTypes = new ArrayList <CarType>();
 		carsTypes = carTypeDao.getAllCarTypes();
-		return carsTypes;
+		if(carsTypes.isEmpty()){
+			throw new EmptyListException();
+		}else
+			return carsTypes;
 	}
-
 }
--- src/test/java/cl/ubb/agil/service/CarTypeServiceTest.java
+++ src/test/java/cl/ubb/agil/service/CarTypeServiceTest.java
@@ -15,6 +15,7 @@ import static org.mockito.Mockito.when;
 
 import cl.ubb.agil.dao.CarTypeDao;
 import cl.ubb.agil.model.CarType;
+import cl.ubb.agil.service.exception.EmptyListException;
 
 @RunWith(MockitoJUnitRunner.class)
 public class CarTypeServiceTest {
@@ -23,22 +24,22 @@ public class CarTypeServiceTest {
 	private	CarTypeDao carTypeDao;
 	
 	@Mock
-	private CarType carType;
+	private CarType carType, carTypeAux;
 	
-	private List<CarType> cars;
+	private List<CarType> listCarsTypes;
 	
 	@InjectMocks
 	private CarTypeService carTypeService;
 	
 	@Before
 	public void setup(){
-		cars = new ArrayList<CarType>();
+		listCarsTypes = new ArrayList<CarType>();
 		
 	}
 	
 	@Test
-	public void getAllCarsTypesReturnListWithOneCar(){
-		cars.add(carType);
+	public void getAllCarsTypesReturnListWithOneCarType() throws EmptyListException{
+		listCarsTypes.add(carType);
 		when(carType.getIdentifier()).thenReturn(1234);
 		when(carType.getName()).thenReturn("sports");
 		when(carType.getTransmisionType()).thenReturn("automatic");
@@ -46,11 +47,27 @@ public class CarTypeServiceTest {
 		when(carType.getBags()).thenReturn(2);
 		when(carType.getPassengers()).thenReturn(2);
 		when(carType.getDailyPrice()).thenReturn(40000);
-		when(carTypeDao.getAllCarTypes()).thenReturn(cars);
+		when(carTypeDao.getAllCarTypes()).thenReturn(listCarsTypes);
 		
-		assertEquals(cars,carTypeService.getAllCarTypes());
+		assertEquals(listCarsTypes,carTypeService.getAllCarTypes());
 		assertEquals(1,carTypeService.getAllCarTypes().size());
 		
+	}
+	
+	@Test
+	public void getAllCarsTypesReturnListWithTwoCarsTypes() throws EmptyListException{
+		listCarsTypes.add(carType);
+		listCarsTypes.add(carTypeAux);
+		when(carTypeDao.getAllCarTypes()).thenReturn(listCarsTypes);
+		
+		assertEquals(2,carTypeService.getAllCarTypes().size());
+	}
+	
+	@Test(expected=EmptyListException.class)
+	public void getAllCarsTypesReturnEmptyList() throws EmptyListException{
+		when(carTypeDao.getAllCarTypes()).thenReturn(listCarsTypes);
+		
+		carTypeService.getAllCarTypes();
 	}
 
 
