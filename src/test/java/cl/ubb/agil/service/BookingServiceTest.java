89b9d7d9ccd0f8bde3a63f2bb268d0bd231af931
Nicolas Oñate Oñate <ing.nionate@gmail.com>
2016-07-02 19:00:45 -0400

    Tercer test BookingService

--- src/test/java/cl/ubb/agil/service/BookingServiceTest.java
+++ src/test/java/cl/ubb/agil/service/BookingServiceTest.java
@@ -55,9 +55,9 @@ public class BookingServiceTest {
 	private BookingService bookingService;
 
 	/*
-	 * El cliente 18770816-8 categor√≠a Persona, reserva el auto con id "1" de la
-	 * sucursal de Santiago, el d√≠a 11/06/2016, a las 15:00 hrs y lo entrega en
-	 * la sucursal de Santiago, el d√≠a 15/06/2016 a las 15:00 hrs. Retorna
+	 * El cliente 18770816-8 categor√≠a Persona, reserva el auto con id "1" de
+	 * la sucursal de Santiago, el d√≠a 11/06/2016, a las 15:00 hrs y lo entrega
+	 * en la sucursal de Santiago, el d√≠a 15/06/2016 a las 15:00 hrs. Retorna
 	 * $40.000.
 	 */
 	@Test
@@ -135,7 +135,7 @@ public class BookingServiceTest {
 		CarType type = new CarType(1, "", "automatic", "diesel", "", 4, 5, 10000);
 		CustomerCategory cCategory = new CustomerCategory(1, "Persona");
 		Customer customer = new Customer(rutCliente, "", "", "", cCategory.getIdentifier());
-		
+
 		when(carTypeDao.getCarType(1)).thenReturn(type);
 		when(cCategoryDao.getCustomerCategoryById(1)).thenReturn(cCategory);
 		when(customerDao.getCustomer(rutCliente)).thenReturn(customer);
@@ -145,11 +145,64 @@ public class BookingServiceTest {
 		when(carSpecDao.get(0)).thenReturn(nissan);
 		when(carSpecDao.get(1)).thenReturn(chevrolet);
 		when(extraDao.get(1)).thenReturn(sillaBebe);
-		
+
 		int resultado = bookingService.booking(rutCliente, santiago.getIdentifier(), startDay, bookingHour,
 				santiago.getIdentifier(), endDay, bookingHour, type.getIdentifier(), extras);
 
 		assertEquals(60000, resultado);
 	}
 
+	/*
+	 * El cliente 18431210-7, categorÌa Persona, reserva el auto con id "3" de
+	 * la sucursal de Santiago, el dÌa 02/06/2016 a las 16:20 hrs y lo entrega
+	 * en la sucursal de Santiago el dÌa 04/06/2016 a las 16:20 hrs, m·s dos
+	 * extras: una "silla para bebÈ" con id "1" y un GPS con id "2". Retorna
+	 * $72.000 ($10.000 auto + $5.000 silla para bebÈ + $3.000 GPS).
+	 */
+	@Test
+	public void shouldReturn72000WhenAClientBookingACarForTwoDaysWithTwoExtras() {
+
+		String rutCliente = "18770816-8";
+		String startDay = "11/06/2016";
+		String endDay = "15/06/2016";
+		String bookingHour = "15:00";
+		Branch santiago = new Branch(1, "Santiago", "");
+		Extra sillaBebe = new Extra(1, "silla para bebe", "", 5000);
+		Extra gps = new Extra(2, "GPS", "", 3000);
+		List<BookingExtra> extras = new ArrayList<>();
+		extras.add(new BookingExtra(1, 1));
+		extras.add(new BookingExtra(1, 2));
+
+		// Cars List
+		List<Car> cars = new ArrayList<>();
+		cars.add(new Car("ASDF-12", "negro", 0));
+		cars.add(new Car("QWER-12", "azul", 2));
+
+		// Car Specification List
+		CarSpecification nissan = new CarSpecification(0, "nissan", "", "");
+		CarSpecification chevrolet = new CarSpecification(1, "chevrolet", "", "");
+		List<CarSpecification> carSpecs = new ArrayList<>();
+		carSpecs.add(nissan);
+		carSpecs.add(chevrolet);
+
+		CarType type = new CarType(1, "", "automatic", "diesel", "", 4, 5, 10000);
+		CustomerCategory cCategory = new CustomerCategory(1, "Persona");
+		Customer customer = new Customer(rutCliente, "", "", "", cCategory.getIdentifier());
+
+		when(carTypeDao.getCarType(1)).thenReturn(type);
+		when(cCategoryDao.getCustomerCategoryById(1)).thenReturn(cCategory);
+		when(customerDao.getCustomer(rutCliente)).thenReturn(customer);
+		when(branchDao.get(1)).thenReturn(santiago);
+		when(carDao.getAllByBranchId(1)).thenReturn(cars);
+		when(carSpecDao.getAllCarsByType(1)).thenReturn(carSpecs);
+		when(carSpecDao.get(0)).thenReturn(nissan);
+		when(carSpecDao.get(1)).thenReturn(chevrolet);
+		when(extraDao.get(1)).thenReturn(sillaBebe);
+		when(extraDao.get(2)).thenReturn(gps);
+
+		int resultado = bookingService.booking(rutCliente, santiago.getIdentifier(), startDay, bookingHour,
+				santiago.getIdentifier(), endDay, bookingHour, type.getIdentifier(), extras);
+
+		assertEquals(72000, resultado);
+	}
 }
