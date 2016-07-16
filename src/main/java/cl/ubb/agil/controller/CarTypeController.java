dcf919d27ee56caf8286038be57f9583e40d2bf1
MiguelCastilloVasquez <m.castillo.vas@gmail.com>
2016-07-15 20:46:40 -0400

    Test completos CarTypeController

--- src/main/java/cl/ubb/agil/controller/CarTypeController.java
+++ src/main/java/cl/ubb/agil/controller/CarTypeController.java
@@ -7,10 +7,16 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.sun.org.apache.xerces.internal.impl.xpath.regex.ParseException;
+
+import cl.ubb.agil.model.BookingExtra;
 import cl.ubb.agil.model.CarType;
 import cl.ubb.agil.service.CarTypeService;
 import cl.ubb.agil.service.exception.EmptyListException;
@@ -36,6 +42,27 @@ public class CarTypeController {
 			response = new ResponseEntity<>(HttpStatus.NOT_FOUND);
 		}
 		return response;
+	}
+	
+	@RequestMapping(value="/value/{identifier}",method = POST)
+	@ResponseBody
+	public ResponseEntity<Integer> priceOfTypeCar(
+			@PathVariable("identifier") int identifier,
+			@RequestParam(value = "startDate", required=true) String startDate,
+			@RequestParam(value = "endDate", required=true) String endDate,
+			final @RequestBody List<BookingExtra> extras
+			){
+				ResponseEntity<Integer> result = null;
+				
+				try{
+					int price = carTypeService.getPrice(startDate, endDate, identifier, extras);
+					result = new ResponseEntity<Integer>(price,HttpStatus.OK);
+				}catch(java.text.ParseException e){
+					System.out.println("Las fechas no están en el formato dd/mm/aaaa");
+				}
+				
+				return result;
+		
 	}
 	
 }
--- src/test/java/cl/ubb/agil/controller/CarTypeControllerTest.java
+++ src/test/java/cl/ubb/agil/controller/CarTypeControllerTest.java
@@ -3,6 +3,7 @@ package cl.ubb.agil.controller;
 
 
 
+import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,8 +12,10 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import static org.mockito.Matchers.anyListOf;
 import static org.mockito.Mockito.when;
 import static org.apache.http.HttpStatus.SC_OK;
 import static org.apache.http.HttpStatus.SC_NOT_FOUND;
@@ -22,7 +25,7 @@ import static org.mockito.Mockito.doThrow;
 import com.jayway.restassured.http.ContentType;
 import com.jayway.restassured.module.mockmvc.RestAssuredMockMvc;
 
-
+import cl.ubb.agil.model.BookingExtra;
 import cl.ubb.agil.model.CarType;
 import cl.ubb.agil.service.CarTypeService;
 import cl.ubb.agil.service.exception.EmptyListException;
@@ -36,6 +39,7 @@ import static com.jayway.restassured.module.mockmvc.RestAssuredMockMvc.given;
 public class CarTypeControllerTest {
 	
 	private List<CarType> myCarTypes = new ArrayList<CarType>();
+	private List<BookingExtra> bkExtras = new ArrayList<BookingExtra>();
 	
 	private CarType carType1,carType2;
 	
@@ -51,6 +55,10 @@ public class CarTypeControllerTest {
 		carType2 = new CarType(2,"familiar","manual","fuel","yes",4,5,35000);
 		myCarTypes.add(carType1);
 		myCarTypes.add(carType2);
+		BookingExtra bkExtra1 = new BookingExtra(1,1);
+		BookingExtra bkExtra2 = new BookingExtra(1,2);
+		bkExtras.add(bkExtra1);
+		bkExtras.add(bkExtra2);
 		RestAssuredMockMvc.standaloneSetup(carTypeController);
 	}
 	
@@ -78,6 +86,43 @@ public class CarTypeControllerTest {
 			get("/cartype/list").
 		then().
 			statusCode(SC_NOT_FOUND);
+	}
+	
+	@Test
+	public void shouldReturnThePriceOfTypeCarInCertainDateWithExtras() throws ParseException{
+		when(carTypeServiceMock.getPrice(
+				Mockito.eq("09/07/2016"),
+				Mockito.eq("11/07/2016"),
+				Mockito.eq(2),
+				anyListOf(BookingExtra.class))).thenReturn(60000);
+		
+		given().
+			contentType(ContentType.JSON).
+			body(bkExtras).
+		when().
+			post("/cartype/value/2/?startDate=09/07/2016&endDate=11/07/2016").
+		then().
+			assertThat().
+			body(equalTo("60000")).
+			statusCode(SC_OK);
+	}
+	
+	@Test
+	public void shouldReturnParseExceptionBecauseDateIsMispelled() throws ParseException{
+		
+		doThrow(new ParseException(null, 0)).when(carTypeServiceMock).getPrice(Mockito.eq("09/07/2016"),
+				Mockito.eq("11/07/2016"),
+				Mockito.eq(2),
+				anyListOf(BookingExtra.class));
+		
+			
+		given().
+			contentType(ContentType.JSON).
+			body(bkExtras).
+		when().
+			post("/cartype/value/2/?startDate=09-07-2016&endDate=11/07/2016").
+		then();
+			
 	}
 	
 	
